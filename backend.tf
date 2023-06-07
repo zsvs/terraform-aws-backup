8d739bda7cba9d6d73e3b1f7499640958ffabc46
SVS <stepanezc@gmail.com>
2023-06-07 18:29:26 +0300

    Added backend to gitignore

--- backend.tf
+++
@@ -1,7 +0,0 @@
-﻿terraform {
-  backend "s3" {
-    bucket = "module-aws-backup"            # Bucket where to SAVE Terraform State
-    key    = "aws-backup/terraform.tfstate" # Object name in the bucket to SAVE Terraform State
-    region = "eu-west-1"                    # Region where bucket created
-  }
-}
